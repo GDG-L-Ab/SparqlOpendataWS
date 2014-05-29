a7e137fbc339c57823e0a68b449d14eabc1a645e
Camelia Boban <aissatech@users.noreply.github.com>
2014-05-29 10:27:27 +0200

    Read from file
    Read SPARQL query from file

--- OpendataSparqlServiceServer/src/org/gdgrome/sparql/ws/server/SparqlQuery.java
+++ OpendataSparqlServiceServer/src/org/gdgrome/sparql/ws/server/SparqlQuery.java
@@ -1,11 +1,11 @@
 package org.gdgrome.sparql.ws.server;
 
 public class SparqlQuery {
-	public String sparqlQueryService(){
+	public String sparqlQueryService() throws IOException {
 		//I want all the cities on the Wikipedia(EN) Category CitiesAndTownsInAbruzzo
 		//where the property populationTotal > 50.000
 		//order by city ASC
-		String sparqlQueryString = 
+		/*String sparqlQueryString = 
 			"PREFIX class: <http://dbpedia.org/class/yago/CitiesAndTownsInAbruzzo> " +
 			"PREFIX property: <http://dbpedia.org/property/populationTotal> " +
 			"SELECT ?resource ?value " + 
@@ -14,32 +14,38 @@ public class SparqlQuery {
 				" ?resource <http://dbpedia.org/property/populationTotal> ?value . " + 
 				" FILTER (?value > 50000) " + 
 				" } " + 
-				" ORDER BY ?resource ?value";
+				" ORDER BY ?resource ?value";*/
 		
-//		//init reader
-//		BufferedReader reader = null;
-//		String line = null;
-//		String sparqlQuery = "";
-//		try {
-//			File file = new File("C://sparqlQueryDbpedia.txt");
-//			reader = new BufferedReader(new FileReader(file));
-//			//defining SPARQL Query			
-//			
-//			//read line
-//			while((line = reader.readLine()) != null){				
-//				sparqlQuery = line;				
-//				System.out.println(sparqlQuery);
-//			}
-//		} catch (FileNotFoundException e) {			
-//			e.printStackTrace();
-//		}
-//		finally {
-//		    try {
-//		        reader.close();
-//		    } catch (IOException e) {
-//		        e.printStackTrace();
-//		    }
-//		}
-		return sparqlQueryString;
+		//init reader
+		BufferedReader reader = null;
+		String line = null;	
+		String sparqlQuery = null;
+		try {
+			//read file
+			File file = new File("C://readSparqlQuery.txt");
+			reader = new BufferedReader(new FileReader(file));
+			//defining SPARQL Query				
+			//read line
+			while((line = reader.readLine()) != null){	
+				if(sparqlQuery == null){
+					sparqlQuery = line;
+				}
+				else{
+					sparqlQuery += line;
+					//System.out.println("server, sparqlQuery -------- " + sparqlQuery);
+				}
+			}
+			//System.out.println("server, sparqlQuery -------- " + sparqlQuery);
+		} catch (FileNotFoundException e) {			
+			e.printStackTrace();
+		}
+		finally {
+		    try {
+		        reader.close();
+		    } catch (IOException e) {
+		        e.printStackTrace();
+		    }
+		}
+		return sparqlQuery;
 	}
 }
